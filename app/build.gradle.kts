d6f5b11a1d82c09d1112da7ee23e01f9ccf75d70
eduvc71-code <eduvc71@gmail.com>
2026-08-03 01:02:44 -0400

    Backup antes de actualizar versiones AGP 9.1 y Kotlin 2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,50 +1,56 @@
-plugins {     id("com.android.application")     id("org.jetbrains.kotlin.android")     id("kotlin-kapt")     id("com.google.dagger.hilt.android")     id("com.google.devtools.ksp") } 
- 
-android { 
-    namespace = "com.multipos.app" 
-    compileSdk = 34 
- 
-    defaultConfig { 
-        applicationId = "com.multipos.app" 
-        minSdk = 24 
-        targetSdk = 34 
-        versionCode = 1 
-        versionName = "1.0" 
-    } 
- 
-    buildTypes { 
-        release { 
-            isMinifyEnabled = false 
-            proguardFiles getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro" 
-        } 
-    } 
-    compileOptions { 
-        sourceCompatibility = JavaVersion.VERSION_17 
-        targetCompatibility = JavaVersion.VERSION_17 
-    } 
-    kotlinOptions { 
-        jvmTarget = "17" 
-    } 
-    buildFeatures { 
-        viewBinding = true 
-    } 
-} 
- 
-dependencies { 
-    implementation("androidx.core:core-ktx:1.12.0") 
-    implementation("androidx.appcompat:appcompat:1.6.1") 
-    implementation("com.google.android.material:material:1.11.0") 
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4") 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6") 
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.6") 
-    implementation("androidx.room:room-runtime:2.6.1") 
-    implementation("androidx.room:room-ktx:2.6.1") 
-    ksp("androidx.room:room-compiler:2.6.1") 
-    implementation("com.google.dagger:hilt-android:2.48.1") 
-    kapt("com.google.dagger:hilt-android-compiler:2.48.1") 
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2") 
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2") 
-    implementation("com.github.bumptech.glide:glide:4.16.0") 
-    ksp("com.github.bumptech.glide:ksp:4.16.0") 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3") 
-} 
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+    id("com.google.devtools.ksp")
+}
+
+android {
+    namespace = "com.multipos.app"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.multipos.app"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+    implementation("com.google.dagger:hilt-android:2.48.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.48.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    ksp("com.github.bumptech.glide:ksp:4.16.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+}
